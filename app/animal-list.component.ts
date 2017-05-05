5428a05ef845f671402417c994afc012552a029f
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-05-05 12:17:31 -0700

    add animal details to list component

--- app/animal-list.component.ts
+++ app/animal-list.component.ts
@@ -4,7 +4,58 @@ import  {Animal} from './animal.model';
 @Component({
   selector: 'animal-list',
   template: ` <h1>Animal List Here</h1>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-2">
 
+      </div>
+      <div class="col-md-2">
+        <h3>Species</h3>
+      </div>
+      <div class="col-md-1">
+        <h3>Name</h3>
+      </div>
+      <div class="col-md-2">
+        <h3>Location</h3>
+      </div>
+      <div class="col-md-2">
+        <h3>Details</h3>
+      </div>
+      <div class="col-md-2">
+        <h3>Likes/Dislikes</h3>
+      </div>
+      <div class="col-md-1">
+
+      </div>
+    </div>
+    <div class="row" *ngFor="let currentAnimal of childAnimalList">
+      <div class="col-md-2">
+        <img src={{currentAnimal.imageUrl}} alt={{currentAnimal.imageAlt}} width="100%">
+      </div>
+      <div class="col-md-2">
+        <h4>{{currentAnimal.species}}</h4>
+      </div>
+      <div class="col-md-1">
+        <h4>{{currentAnimal.name}}</h4>
+      </div>
+      <div class="col-md-2">
+        <h4>{{currentAnimal.location}}</h4>
+      </div>
+      <div class="col-md-2">
+        <p><strong>DOB:</strong> {{currentAnimal.dob}} ({{currentAnimal.ageCaracteristic}})<p>
+        <p><strong>Sex:</strong> {{currentAnimal.sex}}<p>
+        <p><strong>Diet:</strong> {{currentAnimal.diet}}<p>
+        <p><strong>Caretakers:</strong> {{currentAnimal.caretakers}}<p>
+      </div>
+      <div class="col-md-2">
+        <p><strong>Likes:</strong> {{currentAnimal.likes}}<p>
+        <p><strong>Dislikes: </strong> {{currentAnimal.dislikes}}<p>
+      </div>
+      <div class="col-md-1">
+        <button name="button" class="btn btn-primary">Edit</button>
+      </div>
+    </div>
+  </div>
   `
 
 })
@@ -12,5 +63,6 @@ import  {Animal} from './animal.model';
 
 
 export class AnimalListComponent{
-
+@Input()childAnimalList: Animal[];
+@Output()clickSender = new EventEmitter();
 }
--- app/app.component.ts
+++ app/app.component.ts
@@ -5,7 +5,7 @@ import { Animal } from './animal.model'
   selector: 'app-root',
   template: `
   <h1>Zoo Animal Catalog</h1>
-  <animal-list></animal-list>
+  <animal-list [childAnimalList]="masterAnimalList" (clickSender)="editAnimal($event)"></animal-list>
   <animal-edit></animal-edit>
   <animal-new></animal-new>
   `
@@ -17,5 +17,9 @@ export class AppComponent {
     new Animal("Narwhal", "Coco", "07/18/2016", "Young", "Carnivore", "Zone 2", 8, "F", "Rockfish", "Warm temperatures", "https://image.ibb.co/kYRhgQ/narwhal.png", "square icon with narwhal cut-out"),
     new Animal("African lion", "Tobias", "11/23/2004", "Mature", "Carnivore", "Zone 4", 8, "M", "Playing ball", "Sharing food", "https://image.ibb.co/jm6to5/lion.png", "square icon with lion cut-out"),
     new Animal("Flamingo", "Arnie", "02/06/1984", "Senior", "Omnivore", "Zone 1", 3, "M", "Standing in water", "Being disturbed", "https://image.ibb.co/nme2gQ/flamingo.png", "square icon with flamingo cut-out"),
-  ]
+  ];
+  selectedAnimal = null;
+  editAnimal(clickedAnimal: Animal){
+    this.selectedAnimal = clickedAnimal;
+  }
 }
